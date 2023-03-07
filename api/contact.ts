d941c632434cb74dc57ca46eccd94370ec891d19
Rigoberto Perez <rlperez@udel.edu>
2023-03-07 03:18:50 +0000

    add hcaptcha verify call and refactor the call

--- api/contact.ts
+++ api/contact.ts
@@ -1,7 +1,51 @@
-import type {VercelRequest, VercelResponse}from '@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import '@hcaptcha/types';
+import { address } from 'ip';
+import { verify } from 'hcaptcha';
 
-export default function handler(request : VercelRequest, response : VercelResponse,) {
+type CaptchaResponse = {
+	status: number,
+	message: string,
+	error_codes?: string[],
+	hostname?: string,
+};
+
+async function verifyCaptcha(hcaptcha_response: string | undefined): Promise<CaptchaResponse> {
 	const hcaptcha_site_key = process.env.HCAPTCHA_SITE_KEY;
+	const hcaptcha_secret = process.env.HCAPTCHA_SECRET;
 
-    return response.end(`Hello ${hcaptcha_site_key}!`);
+	if (!hcaptcha_secret) {
+		console.error('hCaptcha secret missing!');
+		return { status: 500, message: 'hCaptcha secret key missing!' };
+	}
+
+	if (!hcaptcha_site_key) {
+		console.error('hCaptcha site key missing!');
+		return { status: 500, message: 'hCaptcha site key missing!' };
+	}
+
+	if (hcaptcha_response) {
+		const verify_response = await verify(hcaptcha_secret, hcaptcha_response, address(), hcaptcha_site_key);
+		if (verify_response?.success) {
+			return {
+				status: 200,
+				message: 'hCaptcha verified user',
+			};
+		} else {
+			console.warn({ verify_response });
+			return {
+				status: 429,
+				message: 'hCaptcha verification failed',
+				error_codes: verify_response['error-codes'],
+				hostname: verify_response.hostname
+			};
+		}
+	} else {
+		return { status: 400, message: 'Missing h-captcha-response' };
+	}
+}
+
+export default async function handler(request: VercelRequest, response: VercelResponse,) {
+	const hcaptcha_response = request?.body['h-captcha-response'];
+	const verify_response = await verifyCaptcha(hcaptcha_response);
 }
